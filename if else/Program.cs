bbcda787b4985a9038b9422fa9f21f7596ad7c9f
Glan Artem <artemglan@mail.ru>
2021-02-02 11:08:22 +0400

    Добавьте файлы проекта.

---
+++ if else/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2==1)
+            {
+                Console.WriteLine("Число нечетное");
+            }
+
+            else
+
+            {
+                Console.WriteLine("Число четное");
+            }
+        }
+    }
+}
